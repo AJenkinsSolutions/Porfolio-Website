998edc5812f3b02feea8fc1260e730932c326cd3
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-05-24 15:31:19 -0500

    Add Send Email Functionality, Add Email Envs

--- main.py
+++ main.py
@@ -4,23 +4,41 @@ from wtforms import StringField, IntegerField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
 from dotenv import load_dotenv
 import os
+import smtplib
 
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('APP.SECRET_KEY')
+
+my_email = os.getenv('MY_EMAIL')
+email_password = os.getenv('EMAIL_PASSWORD')
+target_email = os.getenv('TARGET_EMAIL')
 
 #   Test Enviroment variables
 print('PATH', os.getenv('PATH'))
 
 
 class ContactForm(FlaskForm):
-    name = StringField(label='Name', validators=[DataRequired()])
+    name = StringField(label='Name', validators=[DataRequired(), Length(min=2, max=12)])
     email = StringField(label='Email', validators=[DataRequired(), Email()])
     phone = IntegerField(label='Phone', validators=[DataRequired()])
-    message = TextAreaField(label='Message', validators=[DataRequired()])
+    message = TextAreaField(label='Message', validators=[DataRequired(), Length(max=120)])
 
     submit = SubmitField(label='submit')
 
+def send_email(name, email, phone, message):
+    email_message = f"Subject:Portfolio Message ALERT!!\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
+        # tls creates a secure connection
+        connection.starttls()
+        # login using the username and password
+        connection.login(user=my_email, password=email_password)
+        # specify the address you will use and message
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=target_email,
+            # format for email
+            msg=email_message)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -30,7 +48,7 @@ def home():
         email = contact_form.email.data
         phone = contact_form.phone.data
         message = contact_form.message.data
-        print(name, email, phone, message)
+        send_email(name, email, phone, message)
         return render_template('index.html', form=contact_form, msg_sent=True )
     else:
         return render_template("index.html", form=contact_form, msg_sent=False)
