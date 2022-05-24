4307f8e49b95f2f12cdf854a33b0f984d542d599
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-05-24 15:13:19 -0500

    Add DotEnv, Add Os module, Add Hide Sensitive Data functionality

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
-app.secret_key = 'a random string'
+app.secret_key = os.getenv('APP.SECRET_KEY')
+
+#   Test Enviroment variables
+print('PATH', os.getenv('PATH'))
 
 
 class ContactForm(FlaskForm):
@@ -12,6 +18,7 @@ class ContactForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired()])
     phone = IntegerField(label='Phone', validators=[DataRequired()])
     message = TextAreaField(label='Message', validators=[DataRequired()])
+
     submit = SubmitField(label='submit')
 
 
@@ -24,9 +31,8 @@ def home():
         phone = contact_form.phone.data
         message = contact_form.message.data
         print(name, email, phone, message)
-        return render_template('index.html', form=contact_form, msg_sent=True, )
+        return render_template('index.html', form=contact_form, msg_sent=True )
     else:
-
         return render_template("index.html", form=contact_form, msg_sent=False)
 
 
